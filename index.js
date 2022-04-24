9036f310dd535abe964a5983c8af4908da5671fe
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-24 03:35:58 +0000

    commented out fetchMyIP function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { fetchMyIP } = require('./iss');
 
-fetchMyIP((err, ip) => {
-  if (err) {
-    console.log(`It didnt work! ${err}`)
-    return;
-  }
+// fetchMyIP((err, ip) => {
+//   if (err) {
+//     console.log(`It didnt work! ${err}`);
+//     return;
+//   }
 
-  console.log(`It worked! Rturned IP: ${ip}`)
-})
+//   console.log(`It worked! Rturned IP: ${ip}`);
+// });
