d77e9cfd6d24fa01e2fc49a24a3b70279a47080d
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-24 03:36:19 +0000

    cleaned up code

--- iss.js
+++ iss.js
@@ -1,16 +1,23 @@
-const request = require("request")
+const request = require("request");
 
 // fetching IP address
 const fetchMyIP = callback => {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) {
-      return callback(error, null);
+    // error for invalid domain, user offline, etc.
+    if (error) return callback(error, null);
+    
+  
+    // if non-200 status, assume server error.
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
     }
+
+    const ip = JSON.parse(body).ip;
     
-    const data = JSON.parse(body)
-    
-    return callback(null, data.ip)
-  })
+    callback(null, ip);
+  });
 };
 
 module.exports = { fetchMyIP };
