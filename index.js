62a11632b640f27e8a3c89bfe880429af9a84a41
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-24 06:22:26 +0000

    implemented geo coordinates api function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
 
 // fetchMyIP((err, ip) => {
 //   if (err) {
@@ -8,3 +9,12 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log(`It worked! Rturned IP: ${ip}`);
 // });
+
+fetchCoordsByIP('216.71.216.131', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
--- iss.js
+++ iss.js
@@ -20,4 +20,28 @@ const fetchMyIP = callback => {
   });
 };
 
-module.exports = { fetchMyIP };
+// fetching geo coordinates
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+    
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
