f44825cfbbade41da03236fdee300e2a505554bc
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-25 04:11:44 +0000

    implement fetCoordsByIP using promise

--- index2.js
+++ index2.js
@@ -1,5 +1,7 @@
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
   .then(body => console.log(body));
+
 
