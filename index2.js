bf06c5e89dc2be19875e64ec0b7f1f37526ff4d4
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-26 00:27:04 +0000

    add printPasstimes and call nextISSTimesforMyLocation

--- index2.js
+++ index2.js
@@ -1,8 +1,23 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body))
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body))
 
+const printPassTimes = passTimes => {
+  for (const times of passTimes) {
+    const timestamp = times.risetime * 1000;
+    const date = new Date(timestamp);
+    const duration = times.duration;
+    console.log(`Next pass at ${date},for ${duration} seconds!`);
+  }
+};
 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
