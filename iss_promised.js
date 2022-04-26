129ca512cf45219be410e0ff1aaa604073156d6d
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-26 00:21:02 +0000

    complete nextISSForMyLocation with mentor help, rename variables to make more sense

--- iss_promised.js
+++ iss_promised.js
@@ -1,17 +1,41 @@
 const request = require('request-promise-native');
 
-const fetchMyIP = () => {
+const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
-const fetchCoordsByIP = (body) => {
-  const ip = JSON.parse(body).ip
-  return request(`https://freegeoip.app/json/${ip}`);
+const fetchCoordsByIP = ipString => {
+  const ipObject = JSON.parse(ipString).ip;
+  return request(`https://api.ipbase.com/v2/info?apikey=155e98b0-c385-11ec-8582-1d90b85ec049&ip=${ipObject}`);
 };
 
-const fetchISSFlyOverTimes = (body) => {
-  const { latitude, longitude } = JSON.parse(body);
-  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
-}
+const fetchISSFlyOverTimes = coordinates => {
+  const { latitude, longitude } = JSON.parse(coordinates).data.location;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then((ip) => {
+      return fetchCoordsByIP(ip);
+    })
+    .catch((error) => {
+      console.log("It didn't work: coordinates ", error.message);
+    })
+    .then((coordinates) => {
+      return fetchISSFlyOverTimes(coordinates)
+    })
+    .catch((error) => {
+      console.log("It didn't work: Flyover", error.message);
+    })
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response; 
+    })
+    .catch((error) => {
+      console.log("It didn't work: ", error.message);
+    })
+};
+
+module.exports = { nextISSTimesForMyLocation };
